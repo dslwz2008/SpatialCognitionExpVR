a236c1c193eda94b05a900ae4f03fe794cf5b271
dslwz2008 <dslwz2008@gmail.com>
2017-01-14 19:04:11 +0800

    init commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+using VRStandardAssets.Utils;
+
+public class Manager : MonoBehaviour
+{
+    public Text countDownText;
+    public Timer timer;
+
+    public RigidbodyFirstPersonController controller;
+    public GameObject startUI;
+    public SelectionSlider startSlider;
+    public GameObject finishUI;
+    public SelectionSlider finishSlider;
+    public float totalTime = 300.0f;
+
+    void OnEnable()
+    {
+        startSlider.OnBarFilled += StartTiming;
+        finishSlider.OnBarFilled += Exit;
+        timer.OnTimerTicked += UpdateUI;
+        timer.OnTimerFinished += ShowEndUI;
+    }
+
+    void OnDisable()
+    {
+        startSlider.OnBarFilled -= StartTiming;
+        finishSlider.OnBarFilled -= Exit;
+        timer.OnTimerTicked -= UpdateUI;
+        timer.OnTimerFinished -= ShowEndUI;
+    }
+
+    // Use this for initialization
+    void Start () {
+        controller.enabled = false;
+	}
+
+    public void StartTiming()
+    {
+        controller.enabled = true;
+        startUI.SetActive(false);
+        timer.StartTimer(totalTime);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exiting.....");
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    private void UpdateUI()
+    {
+        //Debug.Log(timeLeft.ToString("F0"));
+        countDownText.text = timer.timeLeft.ToString("F0");
+        if (timer.timeLeft < 30.0f)
+        {
+            countDownText.color = Color.red;
+        }
+    }
+
+    private void ShowEndUI()
+    {
+        controller.enabled = false;
+        finishUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class UIController : MonoBehaviour
+{
+    public UIFader m_IntroUI;     // 开始界面
+    public UIFader m_OutroUI;     // 结束界面
+    public UIFader m_PlayerUI;    // 游戏过程中界面
+    //private Text m_GameTime;     // 游戏剩余时间
+    //private Text m_PlayerHealth;      // 玩家生命值
+
+
+    public IEnumerator ShowIntroUI()
+    {
+        yield return StartCoroutine(m_IntroUI.InteruptAndFadeIn());
+    }
+
+
+    public IEnumerator HideIntroUI()
+    {
+        yield return StartCoroutine(m_IntroUI.InteruptAndFadeOut());
+    }
+
+    public void DisableIntroUI() {
+        m_IntroUI.gameObject.SetActive(false);
+    }
+    public void DisableOuttroUI()
+    {
+        m_OutroUI.gameObject.SetActive(false);
+    }
+
+    public IEnumerator ShowOutroUI()
+    {
+        //m_TotalScore.text = SessionData.Score.ToString();
+        //m_HighScore.text = SessionData.HighScore.ToString();
+
+        yield return StartCoroutine(m_OutroUI.InteruptAndFadeIn());
+        m_OutroUI.gameObject.SetActive(true);
+    }
+
+
+    public IEnumerator HideOutroUI()
+    {
+        yield return StartCoroutine(m_OutroUI.InteruptAndFadeOut());
+    }
+
+
+    public IEnumerator ShowPlayerUI()
+    {
+        yield return StartCoroutine(m_PlayerUI.InteruptAndFadeIn());
+    }
+
+
+    public IEnumerator HidePlayerUI()
+    {
+        yield return StartCoroutine(m_PlayerUI.InteruptAndFadeOut());
+    }
+}
